d478c8e19412a71166e60ad87091b6501e433465
Yoon-orn Chin <dylanchin123@gmail.com>
2021-03-04 03:56:37 -0800

    Wrote first test, check input of 5.

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -3,8 +3,7 @@ import fizzbuzz
 
 class TestCase(unittest.TestCase):
     def test1(self):
-        self.assertEqual(fizzbuzz, 'Hello World!')
+        self.assertEqual(fizzbuzz, ['1','2','Fizz','4','Buzz'])
     
-
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
